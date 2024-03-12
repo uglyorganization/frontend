06fe3f23e05713366ccf0d466ac30b1e7ece21b3
Arnau Diaz <diazarnau0@gmail.com>
2024-03-12 11:39:37 +0100

    static data from json

--- src/data/cardData.ts
+++ src/data/cardData.ts
@@ -1,4 +1,3 @@
-// cardData.ts
 export interface CardData {
   id: number;
   href: string;
@@ -6,27 +5,3 @@ export interface CardData {
   body: string;
   voteNumber: number;
 }
-
-export const cardData: CardData[] = [
-  {
-    id: 1,
-    href: 'https://github.com/uglyorganization/frontend',
-    title: 'github - frontend',
-    body: 'Source code for this frontend available here',
-    voteNumber: 10,
-  },
-  {
-    id: 2,
-    href: 'https://www.mediavida.com/foro/dev/the-ugly-organization-we-write-ugly-code-708871',
-    title: '/dev diary',
-    body: '/dev diary',
-    voteNumber: 8
-  },
-  {
-    id: 3,
-    href: 'https://www.arnaudiaz.com',
-    title: 'arnaudiaz blog',
-    body: 'I am a human who fights entropy',
-    voteNumber: 15,
-  },
-];
